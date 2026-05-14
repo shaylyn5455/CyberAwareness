b0db4617096d45ddcb3123508def62deab30f8ae
shaylyn5455 <shaylyn545@gmail.com>
2026-05-14 22:42:12 +0200

    code attribtion

--- CyberAwareness_GUI/MainWindow.xaml.cs
+++ CyberAwareness_GUI/MainWindow.xaml.cs
@@ -3,7 +3,17 @@ using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-
+/* S!--CODE ATTRIBUTION-->
+<!--TITLE: Cyber awareness assistant - Program.cs-->
+<--AUTHOR: (Adnan Yusra)->
+SDATE: (13/05/2026)->
+<--VERSION: (FIREST EDITION) --3
+≤-AVAILABLE:
+(https://advtechonline.sharepoint.com/:w:/r/sites/TertiaryStudents/_layouts/15/Doc.aspx?sour
+/* * REFERENCE: Microsoft Learn (2024) - System.Media.SoundPlayer
+ * URL: https://learn.microsoft.com/en-us/dotnet/api/system.media.soundplayer
+ * Purpose: Implements audio playback for Task 2.
+ */
 namespace CyberAwareness_GUI
 {
     public partial class MainWindow : Window
